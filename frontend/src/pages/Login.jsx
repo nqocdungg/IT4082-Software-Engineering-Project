ec152f37f3da883b68214f13cf9edc211dec20ea
nqocdungg <ngocdungcbnn@gmail.com>
2025-11-06 01:38:16 +0700

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,15 @@
+import { Navigate } from "react-router-dom";
 import Authentication from "../components/Authentication.jsx";
-import Slideshow from "../components/SLideShow.jsx";
-
+import Slideshow from "../components/SlideShow.jsx";
 import "./Login.css";
 
 function Login() {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return <Navigate to="/dashboard" replace />;
+  }
+
   return (
     <div className="login-container">
       <Slideshow />
