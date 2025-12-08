a254cb5091ad3a499338de1e37d48a3be70f2cef
Giang235700 <tg30102005@gmail.com>
2025-12-08 08:26:42 +0700

    update Dashboard

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,22 @@
-import { FaBars, FaBell, FaUser } from "react-icons/fa";
+// src/components/Header.jsx
+import "../styles/header.css";
+import { FiMenu, FiUser } from "react-icons/fi";
 
 export default function Header() {
   return (
     <header className="header">
-      <button className="main-btn">
-        <FaBars />
-      </button>
-      <div className="user">
-        <button className="bell"><FaBell /></button>
-        <button className="user-account"><FaUser /></button>
+      <div className="header-left">
+        <button className="icon-btn">
+          <FiMenu />
+        </button>
+        <span className="logo-text">ADMIN</span>
+      </div>
+
+      <div className="header-right">
+        <span className="welcome-label">Welcome, Admin</span>
+        <button className="icon-btn">
+          <FiUser />
+        </button>
       </div>
     </header>
   );
