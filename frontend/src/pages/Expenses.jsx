50b5d55efbc1a3498780b1dba506fda227c3b242
Giang235700 <tg30102005@gmail.com>
2025-12-16 23:20:54 +0700

    Merge origin/main (keep local Residents)

--- frontend/src/pages/Expenses.jsx
+++ frontend/src/pages/Expenses.jsx
@@ -4,16 +4,11 @@ import SideBar from "../components/SideBar.jsx";
 function Dashboard() {
     return (
         <>
-            <div className="appMain">
-                <Header />
-                <div className="mainContentWrapper">
-                    <SideBar />
                     <div className="mainContent">
                         <h1>Khoản chi</h1>
                         <p>Đây là nội dung chính...</p>
                     </div>
-                </div>
-            </div>
+                
         </>
     )
 }
