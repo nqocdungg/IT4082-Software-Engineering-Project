6b5e00059c17097cc36b079e5475bc354b87a4d3
hangle28042005 <hanglethithu0506@gmail.com>
2025-12-23 04:26:38 +0700

    them resident-frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,10 @@
-import { useNavigate } from "react-router-dom"
-import Authentication from "../components/Authentication"
-import SlideShow from "../components/SlideShow.jsx"
-import "../styles/Login.css"
+import { useNavigate } from "react-router-dom";
+import Authentication from "../components/Authentication";
+import SlideShow from "../components/SlideShow.jsx";
+import "../styles/Login.css";
 
 export default function Login() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="login-container">
@@ -12,15 +12,15 @@ export default function Login() {
       <Authentication
         mode="login"
         onSuccess={() => {
-          const role = localStorage.getItem("role")?.toLowerCase()
+          const role = localStorage.getItem("role")?.toLowerCase();
 
-          if (role === "head") navigate("/dashboard")
-          else if (role === "deputy") navigate("/dashboard")
-          else if (role === "accountant") navigate("/dashboard")
-          else if (role === "household") navigate("/")
-          else navigate("/")
+          if (role === "head") navigate("/dashboard");
+          else if (role === "deputy") navigate("/dashboard");
+          else if (role === "accountant") navigate("/dashboard");
+          else if (role === "household") navigate("/resident-home");
+          else navigate("/");
         }}
       />
     </div>
-  )
+  );
 }
